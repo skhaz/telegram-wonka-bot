617f28ca98da6056ccf2de1e1db18bc0911134be
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-01-31 09:36:21 -0300

    Fix python style

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import os
-import http
 import glob
+import http
+import os
 import random
-
 from typing import Set
 
 import yaml
-
 from flask import Flask, request
-from werkzeug.wrappers import Response
-
 from google.cloud import vision
-
 from telegram import Bot, Update
-from telegram.ext import Dispatcher, Filters, MessageHandler, CallbackContext
+from telegram.ext import CallbackContext, Dispatcher, Filters, MessageHandler
+from werkzeug.wrappers import Response
 
 app = Flask(__name__)
 
@@ -23,7 +19,7 @@ memes = list(glob.glob("memes/*"))
 
 
 with open("keywords.yaml") as f:
-    keywords = set(yaml.load(f, Loader=yaml.FullLoader))
+    keywords = set(yaml.safe_load(f))
 
 
 def words_matcher(phrase: str, words: Set[str], threshold: int = 3) -> bool:
@@ -36,7 +32,7 @@ def photo(update: Update, context: CallbackContext) -> None:
     image = vision.Image(content=bytes(buffer))
 
     response = vision_client.text_detection(image=image)
-    text = " ".join([annotation.description for annotation in response.text_annotations])
+    text = " ".join([a.description for a in response.text_annotations])
     found = words_matcher(text, keywords)
 
     if found:
@@ -50,9 +46,9 @@ bot = Bot(token=os.environ["TOKEN"])
 dispatcher = Dispatcher(bot=bot, update_queue=None, workers=0)
 dispatcher.add_handler(MessageHandler(Filters.photo, photo))
 
+
 @app.route("/", methods=["POST"])
 def index() -> Response:
-    dispatcher.process_update(
-        Update.de_json(request.get_json(force=True), bot))
+    dispatcher.process_update(Update.de_json(request.get_json(force=True), bot))
 
     return "", http.HTTPStatus.NO_CONTENT
